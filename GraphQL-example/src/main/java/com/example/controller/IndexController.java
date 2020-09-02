7d8dbf31416fcc9a009a6853cac14a62daaf0c78
clx <brianchen.clx@gmail.com>
2020-09-03 00:21:59 +0800

    feat:add graphql html

---
+++ GraphQL-example/src/main/java/com/example/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author clx
+ */
+@Controller
+public class IndexController {
+
+	@GetMapping(path = "/index")
+	@ResponseBody
+	public String index() {
+		return "Welcome to GraphQL!";
+	}
+
+	@GetMapping(path = "/api/i")
+	public String graphiql() {
+		return "graphiql";
+	}
+
+	@GetMapping(path = "/api")
+	public String playground() {
+		return "playground";
+	}
+
+	@GetMapping(path = "/api/v")
+	public String voyager() {
+		return "voyager";
+	}
+}
