638e9ced69462075de2fe3181b0627c21b3743d0
clx <brianchen.clx@gmail.com>
2022-06-24 22:59:23 +0800

    fix netty log conf

--- netty-example/netty-example-service/src/main/java/com/example/netty/controller/IndexController.java
+++ netty-example/netty-example-service/src/main/java/com/example/netty/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.netty.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author clx
  */
+@Slf4j
 @RestController
 public class IndexController {
 
 	@GetMapping(path = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String index() {
+		log.info("Welcome to Project example.");
 		return "Welcome to Project example.";
 	}
 }
